060a6a45befabad02f38b09f682a1cef583f076a
primetoxinz <primetoxinzz@gmail.com>
2016-03-02 20:20:39 -0500

    Fixes #5

--- src/org/usfirst/frc/team1793/robot/system/ArmController.java
+++ src/org/usfirst/frc/team1793/robot/system/ArmController.java
@@ -6,40 +6,34 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class ArmController extends Controller {
-	private Arm arm;
+	public static final double OFFSET = -.2;
+
+	private AnalogInput rotaryEncoder = new AnalogInput(Constants.RE_PID);
+	private SpeedController motor;
+
 	public ArmController(SpeedController motor) {
 		super(2);
-		arm = new Arm(motor);
+		this.motor = motor;
 	}
+
 	public void lift(double speed) {
-		executor.execute( () -> arm.lift(speed));
-	}
-	public double getAngle() {
-		return arm.getAngle();
-	}
-	private static class Arm {
-		
-		public static final double OFFSET = -.2;
-		AnalogInput rotaryEncoder = new AnalogInput(Constants.RE_PID);
-		
-		SpeedController motor;
-		public Arm(SpeedController motor) {
-			this.motor = motor;
-		}
-		public void lift(double speed) {
-			if(speed < 0 ) {
-				if(getAngle() < 1)
+		executor.execute(() -> {
+
+			if (speed < 0) {
+				if (getAngle() < 1)
 					motor.set(speed);
-			} else if(speed > 0) {
-				if(getAngle() > .05)
+			} else if (speed > 0) {
+				if (getAngle() > .05)
 					motor.set(speed);
 			} else {
 				motor.set(0);
 			}
-			
-		}
-		public double getAngle() {
-			return rotaryEncoder.getVoltage()-OFFSET;
-		}
+
+		});
 	}
+
+	public double getAngle() {
+		return rotaryEncoder.getVoltage() - OFFSET;
+	}
+
 }
