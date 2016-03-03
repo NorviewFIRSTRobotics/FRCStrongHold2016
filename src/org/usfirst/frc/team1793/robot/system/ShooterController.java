3ccb3d1e8f345f24d11c10922ed2e1ed7d756d5c
primetoxinz <primetoxinzz@gmail.com>
2016-03-02 20:27:23 -0500

    Rework ShooterController similar to ArmController

--- src/org/usfirst/frc/team1793/robot/system/ShooterController.java
+++ src/org/usfirst/frc/team1793/robot/system/ShooterController.java
@@ -5,48 +5,39 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Timer;
 
 public class ShooterController extends Controller {
-	Shooter shooter;
-	private boolean running = false; 
-	public DigitalInput in = new DigitalInput(9);
+	private SpeedController motor;
+	private boolean running;
+	private DigitalInput limitSwitch = new DigitalInput(9);
+
 	public ShooterController(SpeedController motor) {
 		super(1);
-		shooter = new Shooter(motor);
+		this.motor = motor;
 	}
-	public void shoot(double motor) {
-		if(running) {
+
+	public void shoot(double speed) {
+		if (running) {
 			return;
 		}
-		
 		running = true;
-		executor.execute(() -> 
-		{
+		executor.execute(() -> {
 			Timer timer = new Timer();
 			timer.start();
-			while(timer.get() < .25) {
-				System.out.println("Time is "+timer.get());
-				shooter.shoot(-motor);
-			} 
-			while(!on()) {
-				shooter.shoot(motor/4);
-			}
-			shooter.shoot(0);
-			running = false;
-		}
-		);
-	}
-	public boolean on() {
-		return in.get();
-	}
-	private static class Shooter {
-		private SpeedController motor;
-//		private double startTime;
-//		private final double DURATION = 1.5;
-		public Shooter(SpeedController motor) {
-			this.motor = motor;
 			
-		}
-		public void shoot(double motorSpeed) {
-			motor.set(motorSpeed);			
-		}		
+			//Throw ball
+			while (timer.get() < .25) {
+				motor.set(speed);
+			}
+			//Return to store position 
+			while (!inStorePosition()) {
+				motor.set(speed / 4);
+			}
+			motor.set(0);
+			running = false;
+		});
 	}
+
+	public boolean inStorePosition() {
+		return limitSwitch.get();
+	}
+
 }
